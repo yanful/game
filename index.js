ab26e11bd06425578949183081d3be20226f1506
yanful <yanful@uw.edu>
2023-02-10 17:48:43 -0800

    some change

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//https://workshops.hackclub.com/platformer/
 var groundSprites;
 var GROUND_SPRITE_WIDTH = 100;
 var GROUND_SPRITE_HEIGHT = 100;
@@ -29,42 +30,36 @@ window.setup= () => {
     )
     groundSprites.add(groundSprite);
   }
-  mario = createSprite(50,400,20,410);
+  mario = createSprite(50,height-90,50,50);
   mario.addAnimation("running", mario_running);
   mario.scale = 0.5;
   obstacleSprites = new Group();
 }
 window.draw = () => {
   if (isGameOver) {
-    background(0)
-    fill(255)
-    textAlign(CENTER)
-    text('Your score was: ' + score, camera.position.x, camera.position.y - 20)
+    background(0);
+    fill(255);
+    textAlign(CENTER);
+    text('Your score was: ' + score, camera.position.x, camera.position.y - 20);
     text(
       'Game Over! Click anywhere to restart',
       camera.position.x,
       camera.position.y
-    )
+    );
   } else {
     background(150, 200, 250);
     mario.velocity.y = mario.velocity.y + GRAVITY;
     if (groundSprites.overlap(mario)) {
-      mario.velocity.y = 0
-      mario.position.y = height - 50 - mario.height / 2
-    }
+      mario.velocity.y = 0;
+      mario.position.y = (height-75) - (mario.height/2);
+    };
     if (key == 'w') {
       mario.velocity.y = JUMP;
-    } 
+    };
 
     mario.position.x = mario.position.x + 5;
     camera.position.x = mario.position.x + width / 4;
-    var obstacle = createSprite(
-      camera.position.x + width,
-      random(0, height - 70),
-      60,
-      60
-    )
-    var firstObstacle = obstacleSprites[0];
+    var firstGroundSprite = groundSprites[0];
     if (firstGroundSprite.position.x <= camera.position.x - (width/2 + firstGroundSprite.width/2)) {
       groundSprites.remove(firstGroundSprite);
       firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites*firstGroundSprite.width;
@@ -72,14 +67,12 @@ window.draw = () => {
     }
     
     if (random() > 0.95) {
-      var obstacle = createSprite(
-        camera.position.x + width, random(0, (height-50)-15), 30, 30
-      );
+      var obstacle = createSprite(camera.position.x + width,random(200, 400),50,50);
       obstacleSprites.add(obstacle)
     }
 
 
-    var firstGroundSprite = groundSprites[0]
+    var firstObstacle = obstacleSprites[0];
     if (obstacleSprites.length > 0 && firstObstacle.position.x <= camera.position.x - (width/2 + firstObstacle.width/2)) {
       removeSprite(firstObstacle);
     }
