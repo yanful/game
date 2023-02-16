7ba2f3168b1c32be1fdba1dedf2fb4b5713fc269
yanful <yanful@uw.edu>
2023-02-16 15:31:49 -0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   plugins: [
     copy({
-      targets: [{ src: "index.html", dest: "dist" }, { src: "style.css", dest: "dist" }, { src: "Capture1.png", dest: "dist" }, { src: "Capture3.png", dest: "dist" }, { src: "Capture4.png", dest: "dist" }]
+      targets: [{ src: "index.html", dest: "dist" }, { src: "style.css", dest: "dist" }, { src: "Capture1.png", dest: "dist" }, { src: "Capture3.png", dest: "dist" }, { src: "Capture4.png", dest: "dist" }, { src: "node_modules", dest: "dist" }]
     }),
     nodeResolve(),
   ],
